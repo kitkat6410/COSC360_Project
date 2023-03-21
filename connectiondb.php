79c4d1526e9eb722d527a5733012ffed5dac35d9
kitkat6410 <kjbarnes6410@gmail.com>
2023-03-21 13:28:22 -0700

    Changes to profile page and other edits to make it functional

--- connectiondb.php
+++ connectiondb.php
@@ -1,10 +1,12 @@
 <?php
 try{
-
-    // $connString = "mysql:host=127.0.0.1;dbname=culinarycloud";
-    $connString = "mysql:host=cosc360.ok.ubc.ca;dbname=db_93648137";
-    $user = "93648137";
-    $pass = "93648137";
+    // localhost connection string
+    $connString = "mysql:host=127.0.0.1;dbname=culinarycloud";
+    $user = "root";
+    $pass = "";
+    // $connString = "mysql:host=cosc360.ok.ubc.ca;dbname=db_93648137";
+    // $user = "93648137";
+    // $pass = "93648137";
 
     $pdo = new PDO($connString, $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -16,7 +18,7 @@ try{
     //     echo $row['refnum'];
     //     echo "<br/>";
     // }
-    echo "Successfully connected";
+    // echo "Successfully connected";
     // $pdo = null;
 }
 catch (PDOException $e){
--- profile.php
+++ profile.php
@@ -1,22 +1,67 @@
 <?php
 try {
     require 'connectiondb.php';
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    echo $username;
-    echo $password;
+    $user_input = $_POST['username'];
+    $pass_input = $_POST['password'];
     $stmt = $pdo->prepare("SELECT * FROM userinfo WHERE Username = :username AND Password = :password");
-    $stmt->execute(array(':username' => $username, ':password' => $password));
+    $stmt->execute(array(':username' => $user_input, ':password' => $pass_input));
     $row = $stmt->fetch();
-    if ($row) {
-        echo $row['Username'];
-        echo $row['Password'];
-    } else {
-        // echo "Username or password is incorrect.";
+    if (!$row) {
+   // echo "Username or password is incorrect.";
         // Invalid credentials, redirect back to login page with error message
         header('Location: login.php?error=1');
         exit;
-    }
+
+    } 
 } catch (Exception $e) {
     echo ($e);
 }
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Profile Page</title>
+    <link rel="stylesheet" href="css/reset.css">
+    </link>
+    <link rel="stylesheet" href="css/styles.css"></link>
+    <link rel="stylesheet" href="css/profile.css">
+    </link>
+    <nav>
+        <div class="site-title">
+            <a href="home.html">
+                <h1>Culinary Cloud</h1>
+                </a>
+           
+            <p>Social Media Links</p>
+        </div>
+        <ul>
+            <li><a href="blogs.html">Browse Blogs</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="create.html">Create a blog</a></li>
+            <li><a href="index.php">Logout</a></li>
+        </ul>
+    </nav>
+</head>
+
+<body>
+    <h1 class="third-color">Welcome <strong><?php echo $row['Name']?></strong></h1>
+    <div class = "profile fourth-color">
+    <img src=<?php echo $row['ProfileImage']?> alt="Profile Image">
+
+    <div class="details-container">
+    <h2>Account Details</h2>
+    <p> <strong>Username: </strong> <?php echo $row['Username']?></p>
+	<p><strong>Email: </strong> <?php echo $row['Email']?></p>
+	<p><strong>Birth Date: </strong><?php echo $row['BirthDate']?></p>
+	<p><strong>Account Created: </strong><?php echo $row['AccountCreated']?></p>
+</div>
+</div>
+
+
+	
+
+</body>
+
+</html>
