fa9339a0a406624215e6fe1bb0d9d113b2669df4
kitkat6410 <kjbarnes6410@gmail.com>
2023-03-26 22:32:48 -0700

    deleted a comment

--- SessionValidation.php
+++ SessionValidation.php
@@ -7,7 +7,5 @@ if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > 
     // session has expired
     session_unset();     // unset $_SESSION variable for this page
     session_destroy();   // destroy session data
-    // header('Location: login.php'); // redirect to login page
-    // exit;
 }
 ?>
